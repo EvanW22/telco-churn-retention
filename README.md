faf6b8813f22ce494784febf0e960921eb9be39d
EvanW22 <evanwill0112@gmail.com>
2026-02-19 00:17:57 -0500

    adjusted models to correct overfitting and chose a new final model. Also started readme.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# telco-churn-retention
-End-to-end data science project using machine learning to predict customer churn and optimize retention decisions under cost constraints.
+# Telecom Customer Churn and Retention
+
+### Section 1 - Project Summary
+Built a customer churn prediction model for a telecommunications dataset and created a retention simulation to maximize profit. The final model achieved a PR-AUC of 0.673 (26.5% churn rate) and generated an estimated $28.7k in expected retention profit from 506 targeted customers.
+
+### Section 2 - Data and Problem Statement
+Given a dataset representing customer subscription and billing data from a telecom provider, predict customer churn. Then, identify high-risk customers and deploy a retention strategy to optimize retention spending and future customer value.
+
+### Section 3 - Modeling Approach
+x
+
+### Section 4 - Modeling Results and Evaluation
+x
+
+## Section 5 - Retention and Business Impact
+x
--- src/models.py
+++ src/models.py
@@ -5,18 +5,42 @@ from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 def get_models():
     return {
         "logistic": LogisticRegression(
-            max_iter=1000
+            max_iter=1000,
+            C=0.3,
+            penalty="l2"
         ),
         "xgboost": XGBClassifier(
             eval_metric="logloss",
+            max_depth=3,              
+            learning_rate=0.03,       
+            n_estimators=600,         
+            subsample=0.8,            
+            colsample_bytree=0.8,     
+            reg_alpha=2,              
+            reg_lambda=3,             
+            min_child_weight=10,      
+            gamma=1,                  
             random_state=42
         ),
         "random_forest": RandomForestClassifier(
             n_estimators=300,
+            max_depth=5,               
+            min_samples_leaf=25,       
+            min_samples_split=50,      
+            max_features="sqrt",       
+            bootstrap=True,            
             random_state=42,
             n_jobs=-1
         ),
         "gbm_sklearn": GradientBoostingClassifier(
+            n_estimators=400,          
+            learning_rate=0.03,        
+            max_depth=3,               
+            min_samples_leaf=25,       
+            subsample=0.8,             
+            max_features="sqrt",       
+            validation_fraction=0.1,   
+            n_iter_no_change=10,       
             random_state=42
         )
     }
